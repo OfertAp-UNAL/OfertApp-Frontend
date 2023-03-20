87667cf332d83612de1894be5d24660cdad2dfd3
Jose <joavila@unal.edu.co>
2023-03-19 23:49:02 -0500

    Rendered NavBar logo

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
+import logo from "../images/Ofertapp.png";
 
 const NavBar = ({ user }) => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Link className="navbar-brand" to="/">
-        OfertApp
+        <img src={logo} alt="Nope" style={{ width: "25%" }} />
       </Link>
       <button
         className="navbar-toggler"
@@ -19,7 +20,11 @@ const NavBar = ({ user }) => {
         <span className="navbar-toggler-icon" />
       </button>
       <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div className="navbar-nav"></div>
+        <div className="navbar-nav">
+          <NavLink className="nav-item nav-link" to="/homepage">
+            Inicio
+          </NavLink>
+        </div>
       </div>
     </nav>
   );
