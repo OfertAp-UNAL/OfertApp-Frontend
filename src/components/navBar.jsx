5fa61700c5c4b23cbdb32f750c51bafb1b7d50ab
Jose <joavila@unal.edu.co>
2023-03-23 15:11:39 -0500

    Make navbar responsive.

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,30 +1,42 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import logo from "../images/Ofertapp.png";
 
 const NavBar = ({ user }) => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-sm navbar-light bg-light">
       <Link className="navbar-brand" to="/">
-        <img src={logo} alt="Nope" style={{ width: "25%" }} />
+        <img
+          className="d-inline-block align-top"
+          src={logo}
+          alt="Nope"
+          style={{ width: "25%" }}
+        />
       </Link>
       <button
         className="navbar-toggler"
         type="button"
-        data-toggle="collapse"
-        data-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
         <span className="navbar-toggler-icon" />
       </button>
-      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div className="navbar-nav">
-          <NavLink className="nav-item nav-link" to="/homepage">
-            Inicio
-          </NavLink>
-        </div>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link className="nav-link" to="/homepage">
+              Inicio
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/subastas">
+              Subastas
+            </Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
