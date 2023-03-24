3fabaaa973e3f51a72705d83cebcaf8d68f49973
decheverri@unal.edu.co <decheverri@unal.edu.co>
2023-03-24 17:13:21 -0500

    Algunos cambios esteticos a la barra de navegacion, aun no esta terminado

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import logo from "../images/Ofertapp.png";
+import "../App.css";
 
 const NavBar = ({ user }) => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg">
       <Link className="navbar-brand" to="/">
         <img src={logo} alt="Nope" style={{ width: "25%" }} />
       </Link>
@@ -18,13 +19,18 @@ const NavBar = ({ user }) => {
         aria-label="Toggle navigation"
       >
         <span className="navbar-toggler-icon" />
-      </button>
+      </button >
       <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div className="navbar-nav">
           <NavLink className="nav-item nav-link" to="/homepage">
             Inicio
           </NavLink>
         </div>
+      </div>
+      <div>
+        <button className="login-button" type="button">
+          Iniciar sesion
+        </button>
       </div>
     </nav>
   );
