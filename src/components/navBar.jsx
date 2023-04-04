42f89c4ef7f2bd306a03275965c4f281f998805d
decheverri@unal.edu.co <decheverri@unal.edu.co>
2023-04-04 02:53:21 -0500

    cambios decorativos y responsive de la barra de navegacion.

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -2,15 +2,40 @@ import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../images/Ofertapp.png";
 import "../App.css";
+import "./navBar.css";
 
 const NavBar = ({ user }) => {
   return (
-    <nav className="navbar navbar-expand-lg">
+    <nav className="navbar navbar-expand-lg" style={{ flexWrap: "nowrap"}}>
       <Link className="navbar-brand" to="/">
         <img className="navbar-logo" src={logo} alt="Nope" />
       </Link>
+      <div className="collapse navbar-collapse show" id="navbarNav">
+        <ul className="navbar-nav row">
+          <li className="nav-item col-12 col-md-1 simple-space">
+            <Link className="nav-link">
+              
+            </Link>
+          </li>
+          <li className="nav-item col-12 col-md-5">
+            <Link className="nav-link" to="/homepage">
+              Inicio
+            </Link>
+          </li>
+          <li className="nav-item col-12 col-md-5">
+            <Link className="nav-link" to="/subastas">
+              Subastas
+            </Link>
+          </li>
+          <li className="nav-item col-12 col-md-1">
+            <Link className="nav-link">
+              
+            </Link>
+          </li>
+        </ul>
+      </div>
       <button
-        className="navbar-toggler"
+        className="navbar-toggler me-3"
         type="button"
         data-bs-toggle="collapse"
         data-bs-target="#navbarNav"
@@ -20,21 +45,7 @@ const NavBar = ({ user }) => {
       >
         <span className="navbar-toggler-icon" />
       </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link className="nav-link" to="/homepage">
-              Inicio
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/subastas">
-              Subastas
-            </Link>
-          </li>
-        </ul>
-      </div>
-      <button className="green-button" type="button">
+      <button className="green-button me-3" type="button">
         Iniciar sesion
       </button>
     </nav>
