b5472d8b655d1ccd3e7aa35160c5430dfc561ea4
decheverri@unal.edu.co <decheverri@unal.edu.co>
2023-04-23 23:23:58 -0500

    Resolucion de merge

--- src/components/Publication/publicationViewOffers.jsx
+++ src/components/Publication/publicationViewOffers.jsx
@@ -2,15 +2,10 @@ import React, { Component } from 'react';
 import Offer from '../common/Offer/offer';
 import CustomButton from '../common/Button/button';
 import './publicationView.css';
-import Form from "../common/form";
-import Joi from "joi-browser";
 
 class PublicationViewOffers extends Component {
 
     state = {
-        data: {
-            bid: 0
-        },
         offers : []
     }
 
@@ -18,7 +13,7 @@ class PublicationViewOffers extends Component {
         const { publication } = this.props;
         this.setState({offers: publication.offers});
     }
-
+    
     render(){
 
         // Useful vars
@@ -48,41 +43,7 @@ class PublicationViewOffers extends Component {
                     :
                     <p className = "ofertapp-label">No hay ofertas</p>
                 }
-<<<<<<< HEAD
-                </div>
-                <button type="button" className="btn ofertapp-button-primary" data-toggle="modal" data-target="#modalOferta">
-                    Crear oferta
-                </button>
-                <div className="modal fade" id="modalOferta" tabIndex="-1" role="dialog" aria-labelledby="modalOfertaLabel" aria-hidden="true">
-                    <div className="modal-dialog" role="document">
-                        <div className="modal-content">
-                            <div className="modal-header">
-                                <h5 className="modal-title" id="exampleModalLabel">Oferta</h5>
-                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div class="modal-body">
-                                <div className="row">
-                                    <div className="col-6">
-                                        <label htmlFor='bid' className="form-label">Cantidad a ofertar</label>
-                                    </div>
-                                    <div className="col-6">
-                                        <input name="bid" id="bid" className="form-control" />
-                                    </div>
-                                </div>
-                                <div className="row align-right pt-3">
-                                    <button type="button" className="btn ofertapp-button-primary">
-                                        Postular oferta
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-=======
                 <CustomButton onClick = {() => console.log("Crear Oferta")} caption = "Crear Oferta" type = "primary"/>
->>>>>>> 94c74112306b7884afc17ef0f3f185135e201e3f
             </div>
         )
     }
