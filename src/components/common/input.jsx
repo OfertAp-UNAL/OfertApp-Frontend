4d9c4b38f4a27cbc7d03af79bec6cddefa307e16
Jose <joavila@unal.edu.co>
2023-04-27 18:04:21 -0500

    Login form re-styled

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Input = ({ name, label, error, ...rest }) => {
   return (
-    <div className="form-group mb-3">
-      <label htmlFor={name} className="form-label">{label}</label>
-      <input {...rest} name={name} id={name} className="form-control" />
+    <div className="group-form">
+      <label htmlFor={name}>{label}</label>
+      <input {...rest} name={name} id={name} />
       {error && <div className="alert alert-danger">{error}</div>}
     </div>
   );
