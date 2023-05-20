9de2d789e8e2d949fbd8bed6975df4521bd9db77
decheverri@unal.edu.co <decheverri@unal.edu.co>
2023-05-20 10:08:00 -0500

    responsive notifications

--- src/components/common/Notification/notification.jsx
+++ src/components/common/Notification/notification.jsx
@@ -16,15 +16,15 @@ class Notification extends Component {
             <div className={ "notification-container" +
             (notification.isRead ? " notification-read" : "")}>
               <div className="row" style = {{"font-weight": "bold"}}>
-                <div className="col-10 offset-1">
+                <div className="col-10 offset-1 pt-1 pb-2">
                   {notification.title}
                 </div>
               </div>
               <div className="row">
-                <div className="col-9">
+                <div className="col-9 pb-3">
                   {notification.description}
                 </div>
-                <div className="col-3" style = {{"display": "flex", "justify-content": "flex-end", "align-items": "flex-end"}}>
+                <div className="col-3" style = {{"display": "flex", "justify-content": "flex-end", "align-items": "flex-end", "font-size": "small"}}>
                 Hace {getTimeLeft(notification.createdAt, true)}
                 </div>
               </div>
