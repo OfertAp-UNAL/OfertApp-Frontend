49c15381f995309b2d9f39260ca7289864854005
decheverri@unal.edu.co <decheverri@unal.edu.co>
2023-05-29 03:07:00 -0500

    small change for user edit page

--- src/components/UserInfoEdit/userInfoEdit.jsx
+++ src/components/UserInfoEdit/userInfoEdit.jsx
@@ -236,9 +236,11 @@ class UserInfoEdit extends Form {
       } else {
         this.setState({ serverErrors: error })
         toast.error("Error registrando usuario, verifique los campos digitados");
+        document.activeElement.blur();
       }
     } catch( e ) {
       toast.error("Error registrando usuario, verifique los campos digitados");
+      document.activeElement.blur();
     }
   }
 
