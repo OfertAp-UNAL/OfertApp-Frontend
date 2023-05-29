9f778103f9d7627f1d8ed9b62178228f93813100
decheverri@unal.edu.co <decheverri@unal.edu.co>
2023-05-29 04:09:00 -0500

    error notifications for register page -> in english

--- src/components/UserInfoEdit/userInfoEdit.jsx
+++ src/components/UserInfoEdit/userInfoEdit.jsx
@@ -171,6 +171,22 @@ class UserInfoEdit extends Form {
       formData.append("profilePicture", user.profilePicture);
     }
 
+    document.getElementById("idError").innerHTML = "";
+    document.getElementById("firstNameError").innerHTML = "";
+    document.getElementById("lastNameError").innerHTML = "";
+    document.getElementById("usernameError").innerHTML = "";
+    document.getElementById("emailError").innerHTML = "";
+    document.getElementById("birthdateError").innerHTML = "";
+    document.getElementById("phoneError").innerHTML = "";
+    document.getElementById("passwordError").innerHTML = "";
+    document.getElementById("confirmPasswordError").innerHTML = "";
+    document.getElementById("profilePictureError").innerHTML = "";
+    document.getElementById("departmentError").innerHTML = "";
+    document.getElementById("municipalityError").innerHTML = "";
+    document.getElementById("addressError").innerHTML = "";
+    document.getElementById("paymentAccountTypeError").innerHTML = "";
+    document.getElementById("paymentAccountNumberError").innerHTML = "";
+
     // Now perform our request
     try{
       const response = await registerUser(formData);
@@ -185,8 +201,54 @@ class UserInfoEdit extends Form {
         this.props.navigate("/homepage");
       } else {
         this.setState({ serverErrors: error })
+        console.log(error);
         toast.error("Error registrando usuario, verifique los campos digitados");
         document.activeElement.blur();
+        if ("id" in error) {
+          document.getElementById("idError").innerHTML = error.id[0]
+        }
+        if ("firstName" in error) {
+          document.getElementById("firstNameError").innerHTML = error.firstName[0]
+        }
+        if ("lastName" in error) {
+          document.getElementById("lastNameError").innerHTML = error.lastName[0]
+        }
+        if ("username" in error) {
+          document.getElementById("usernameError").innerHTML = error.username[0]
+        }
+        if ("email" in error) {
+          document.getElementById("emailError").innerHTML = error.email[0]
+        }
+        if ("birthdate" in error) {
+          document.getElementById("birthdateError").innerHTML = error.birthdate[0]
+        }
+        if ("phone" in error) {
+          document.getElementById("phoneError").innerHTML = error.phone[0]
+        }
+        if ("password" in error) {
+          document.getElementById("passwordError").innerHTML = error.password[0]
+        }
+        if ("confirmPassword" in error) {
+          document.getElementById("confirmPasswordError").innerHTML = error.confirmPassword[0]
+        }
+        if ("profilePicture" in error) {
+          document.getElementById("profilePictureError").innerHTML = error.profilePicture[0]
+        }
+        if ("department" in error) {
+          document.getElementById("departmentError").innerHTML = error.department[0]
+        }
+        if ("municipality" in error) {
+          document.getElementById("municipalityError").innerHTML = error.municipality[0]
+        }
+        if ("address" in error) {
+          document.getElementById("addressError").innerHTML = error.address[0]
+        }
+        if ("paymentAccountType" in error) {
+          document.getElementById("paymentAccountTypeError").innerHTML = error.paymentAccountType[0]
+        }
+        if ("paymentAccountNumber" in error) {
+          document.getElementById("paymentAccountNumberError").innerHTML = error.paymentAccountNumber[0]
+        }
       }
     } catch( e ) {
       toast.error("Error registrando usuario, verifique los campos digitados");
@@ -348,33 +410,33 @@ class UserInfoEdit extends Form {
               "OBLIGATORIO: Buscamos garantizar que nuestros usuarios sean personas reales, " +
               "por lo que necesitamos tu número de cédula para verificar tu identidad"
             )}
-            <div className="ofertapp-div-hline"></div>
+            <div className="ofertapp-div-hline" id="idError"></div>
             {this.renderInput(
               "firstName", "Tu(s) nombre(s)", "text",
               false, "", firstName,
               "OBLIGATORIO: Escribe los nombres en tu documento de identidad"
             )}
-            <div className="ofertapp-div-hline"></div>
+            <div className="ofertapp-div-hline"  id="firstNameError"></div>
             {this.renderInput(
               "lastName", "Tu(s) apellido(s)", "text",
               false, "", lastName,
               "OBLIGATORIO: Escribe los apellidos en tu documento de identidad"
             )}
-            <div className="ofertapp-div-hline"></div>
+            <div className="ofertapp-div-hline"  id="lastNameError"></div>
             {this.renderInput(
               "username", "Nombre de usuario, recuerda que debe ser único", "text",
               editing, "", username,
               "OBLIGATORIO: Éste nombre de usuario aparecerá en la mayoría de acciones" +
               "que realices en OfertApp, por lo que debe ser único y fácil de recordar"
             )}
-            <div className="ofertapp-div-hline"></div>
+            <div className="ofertapp-div-hline"  id="usernameError"></div>
             {this.renderInput(
               "email", "Tu correo electrónico, recuerda que debe ser único", "email",
               editing, "", email,
               "OBLIGATORIO: Te enviaremos un correo de confirmación al email que digites" +
               ". También podrías recibir mas notificiaciones de OfertApp en este correo"
             )}
-            <div className="ofertapp-div-hline"></div>
+            <div className="ofertapp-div-hline"  id="emailError"></div>
             {this.renderInput(
               "birthdate", "¿Cúando naciste?", "date", 
               false, "", editing ? birthdate :
@@ -384,13 +446,13 @@ class UserInfoEdit extends Form {
               "ofrecerte una mejor experiencia en OfertApp y verificar que eres mayor de edad"
               )
             }
-            <div className="ofertapp-div-hline"></div>
+            <div className="ofertapp-div-hline"  id="birthdateError"></div>
             {this.renderInput(
               "phone", "Tu número de teléfono", "text",
               editing, "", phone, 
               "OBLIGATORIO: Necesitamos tu número de teléfono para poder confirmar tu identidad"
             )}
-            <div className="ofertapp-div-hline"></div>
+            <div className="ofertapp-div-hline"  id="phoneError"></div>
             <h1 className = "ofertapp-inspirational-message">
               {editing ? "Actualiza tu contraseña" : "Crea una contraseña" }
             </h1>
@@ -400,7 +462,7 @@ class UserInfoEdit extends Form {
               (editing ? "OPCIONAL" : "OBLIGATORIO") + ": Tu contraseña debe tener al menos 8 caracteres, " +
               "una letra mayúscula, una letra minúscula y un número"
             )}
-            <div className="ofertapp-div-hline"></div>
+            <div className="ofertapp-div-hline"  id="passwordError"></div>
             {this.renderInput(
               "confirmPassword",
               "Confirmar Contraseña",
@@ -408,7 +470,7 @@ class UserInfoEdit extends Form {
               (editing) ? "Si deseas cambiar tu contraseña, recuerda que debe coincidir con la anterior" :
               "OBLIGATORIO: Debe coincidir con la contraseña anterior"
             )}
-            <div className="ofertapp-div-hline"></div>
+            <div className="ofertapp-div-hline"  id="confirmPasswordError"></div>
           </div>
           <div className="col-12 col-md-6 ofertapp-registration-column">
             <FileUpload 
@@ -417,7 +479,7 @@ class UserInfoEdit extends Form {
               info = {"OPCIONAL: Puedes subir una imagen de perfil para que los demás usuarios " +
               "te reconozcan más fácilmente"}
             />
-            <div className="ofertapp-div-hline"></div>
+            <div className="ofertapp-div-hline"  id="profilePictureError"></div>
             {this.renderAutosuggest(
               "department",
               "Departamento",
@@ -426,7 +488,7 @@ class UserInfoEdit extends Form {
               "OBLIGATORIO: Necesitamos saber en qué departamento vives para poder " +
               "ofrecerte una mejor experiencia en OfertApp"
             )}
-            <div className="ofertapp-div-hline"></div>
+            <div className="ofertapp-div-hline"  id="departmentError"></div>
             {this.renderAutosuggest(
               "municipality",
               "Municipio",
@@ -434,14 +496,14 @@ class UserInfoEdit extends Form {
               this.handleMunicipalitySelection,
               "OBLIGATORIO: El municipio del departamento donde vives"
             )}
-            <div className="ofertapp-div-hline"></div>
+            <div className="ofertapp-div-hline"  id="municipalityError"></div>
             {this.renderInput(
               "address", "Dirección", "text",
               editing, "", address,
               "OPCIONAL: Tu dirección será relevante para revisar cualquier tipo de " +
               "transacción que realices en OfertApp"
             )}
-            <div className="ofertapp-div-hline"></div>
+            <div className="ofertapp-div-hline"  id="addressError"></div>
             <ComboBox
               label="Tipo de cuenta"
               options={paymentTypeOptions}
@@ -456,14 +518,14 @@ class UserInfoEdit extends Form {
                 "verificar tu identidad cuando realizas transacciones en OfertApp"
               }
             />
-            <div className="ofertapp-div-hline"></div>
+            <div className="ofertapp-div-hline"  id="paymentAccountTypeError"></div>
             {this.renderInput(
               "paymentAccountNumber", "Número de cuenta", "text",
               editing, "", paymentAccountNumber,
               "OBLIGATORIO: Necesitamos saber el número de tu cuenta bancaria para poder " +
               "verificar tu identidad cuando realizas transacciones en OfertApp"
             )}
-            <div className="ofertapp-div-hline"></div>
+            <div className="ofertapp-div-hline"  id="paymentAccountNumberError"></div>
             {
               !editing && this.renderTermsConditionsCheckbox()
             }
